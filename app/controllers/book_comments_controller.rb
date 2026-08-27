757ba8b8a3e633ac4fa0003725a6f77b5b4558fe
rinatannn <311713641+rinatannn@users.noreply.github.com>
2026-08-27 05:04:01 +0000

    Add asynchronous favorite and comment functionality

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,23 +1,20 @@
 class BookCommentsController < ApplicationController
-  def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+  before_action :authenticate_user!
 
-    if comment.save
-      redirect_back(fallback_location: book_path(book))
-    else
-      redirect_back(fallback_location: book_path(book))
-    end
+  def create
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.save
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.find_by(id: params[:id], book_id: book.id)
-
-    comment&.destroy
-
-    redirect_back(fallback_location: book_path(book))
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.find_by(
+      id: params[:id],
+      book_id: @book.id
+    )
+    @book_comment&.destroy
   end
 
   private
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,14 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
-    favorite.save
-    redirect_back(fallback_location: books_path)
+    @book = Book.find(params[:book_id])
+    current_user.favorites.create(book_id: @book.id)
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite&.destroy
-    redirect_back(fallback_location: books_path)
   end
 end
